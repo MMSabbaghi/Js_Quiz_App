0aa3f2aeb171b52ee3f535778f2df083e3cb47dd
Your Name <you@example.com>
2021-07-28 23:14:30 +0430

    first commit

---
+++ Db.js
@@ -0,0 +1,32 @@
+const dbQuestions = [
+    {
+        id: 1,
+        description: "چند درصد سطح زمین آب است؟",
+        choices: [
+            { questionId: 1, slot: 1, text: "75", isCorrect: true },
+            { questionId: 1, slot: 2, text: "65", isCorrect: false },
+            { questionId: 1, slot: 3, text: "55", isCorrect: false },
+            { questionId: 1, slot: 4, text: "45", isCorrect: false },
+        ]
+    },
+    {
+        id: 2,
+        description: "کدام زبان برنامه نویسی برای هوش مصنوعی مناسب تر است؟",
+        choices: [
+            { questionId: 2, slot: 1, text: "سی شارپ", isCorrect: false },
+            { questionId: 2, slot: 2, text: "پایتون", isCorrect: true },
+            { questionId: 2, slot: 3, text: "جاوا", isCorrect: false },
+            { questionId: 2, slot: 4, text: "جاوااسکریپت", isCorrect: false },
+        ]
+    },
+    {
+        id: 3,
+        description: "واحد پول ژاپن کدام است؟",
+        choices: [
+            { questionId: 3, slot: 1, text: "یوان", isCorrect: true },
+            { questionId: 3, slot: 2, text: "ین", isCorrect: false },
+            { questionId: 3, slot: 3, text: "دلار ژاپن", isCorrect: false },
+            { questionId: 3, slot: 4, text: "یورو", isCorrect: false },
+        ]
+    },
+]
